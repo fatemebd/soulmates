8176dbac465d3fd03d422ed206a2c2683102fe79
tiredkoala-e <fateme.s.e.81@gmail.com>
2021-05-11 23:05:41 +0430

    Add main code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,114 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+
+
+
+class node{
+
+ private:
+
+  int data;
+
+  node* next;
+
+  node* prev;
+
+ public:
+
+  void set_next(node * nx)
+      {
+          next=nx;
+      }
+      node * get_next()
+      {
+          return this->next;
+      }
+      int get_data()
+      {
+          return data;
+      }
+      void set_data(int data)
+      {
+          this->data=data;
+      }
+      void set_prev(node * pr)
+          {
+              prev=pr;
+          }
+          node * get_prev()
+          {
+              return this->prev;
+          }
+  };
+
+
+class Circular_linked_list{
+
+ private:
+
+  int size;
+
+  node* current;
+
+ public:
+
+  Circular_linked_list()
+  {
+      size=0;
+      current=nullptr;
+  }
+
+  ~Circular_linked_list();
+
+  void set_current(node * current)
+      {
+      this->current=current;
+      }
+      node * get_current()
+      {
+          return this->current;
+      }
+
+  void push();
+
+  void pop();
+
+  void pop(int data);
+
+  void push_after(int new_data,int pa_data);
+
+  void push_befor(int new_data,int pb_data);
+
+  void print();
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
 }
